da9751d6e8f5122c3f955bb1b9c7af99f92e35c3
yu <yu.exclusive@icloud.com>
2022-12-08 14:45:22 +0800

    remove psql config

--- src/pg.rs
+++ src/pg.rs
@@ -3,10 +3,6 @@
 use async_once::AsyncOnce;
 use lazy_static::lazy_static;
 use sqlx::{Pool, Postgres, Transaction};
-use std::fmt;
-use std::fs::{self, File};
-use std::io::Write;
-use std::sync::Mutex;
 use std::{env, result::Result};
 
 pub type SqlResult<T, E = sqlx::Error> = Result<T, E>;
@@ -15,47 +11,6 @@ pub type Executor = Pool<Postgres>;
 
 pub async fn tran<'a>() -> SqlResult<Transaction<'a, Postgres>> {
     CONN.get().await.begin().await
-}
-
-#[derive(Clone)]
-pub struct Config {
-    user: String,
-    pwd: String,
-    host: String,
-    port: String,
-    db: String,
-}
-
-lazy_static! {
-    static ref CONFIG: Mutex<Option<Config>> = Default::default();
-}
-
-pub fn init(user: &str, pwd: &str, host: &str, port: u16, db: &str) {
-    *CONFIG.lock().unwrap() = Some(Config {
-        user: user.to_string(),
-        pwd: pwd.to_string(),
-        host: host.to_string(),
-        port: port.to_string(),
-        db: db.to_string(),
-    });
-
-    let url = CONFIG.lock().unwrap().clone();
-
-    if let Ok(_) = fs::try_exists("./.env") {
-        let mut file = File::create_new(".env").unwrap();
-        let bs = format!("DATABADE_URL=\"{}\"", url.unwrap().to_string()).as_bytes();
-        file.write(bs).unwrap();
-        file.flush().unwrap();
-    }
-}
-
-impl fmt::Display for Config {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.user, self.pwd, self.host, self.port, self.db,
-        ))
-    }
 }
 
 lazy_static! {
