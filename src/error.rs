ea06ded371c892c35474ac626948c3c30c5202a6
yu <yu.exclusive@icloud.com>
2023-01-10 12:00:59 +0800

    fix

--- src/error.rs
+++ src/error.rs
@@ -104,6 +104,7 @@ impl actix_web::error::ResponseError for ErrorKind {
     }
 
     fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        log::error!("{}", self);
         HttpResponse::build(self.status_code())
             .insert_header(ContentType::html())
             .insert_header(("access-control-allow-origin", "*"))
