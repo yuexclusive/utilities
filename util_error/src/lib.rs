7fe94b678987fb1bc757c5be4ee7027c47fd92d8
yu <yu.exclusive@icloud.com>
2023-11-06 14:31:46 +0800

    fix lettre 11.1 error

--- util_error/src/lib.rs
+++ util_error/src/lib.rs
@@ -5,7 +5,7 @@ pub enum ErrCode {
     Business = 500000,
     Validate = 400000,
     Unauthorized = 401000,
-    // Hint = 452000,
+    Hint = 452000,
     Timeout = 408000,
     Other = 600000,
 }
@@ -21,8 +21,8 @@ pub enum ErrorKind {
     #[error("[ err_code: {} ] unauthorized: {}",.err_code,.msg)]
     Unauthorized { msg: String, err_code: usize },
 
-    #[error("hint: {}",.0)]
-    Hint(String),
+    #[error("[ err_code: {} ] hint: {}",.err_code,.msg)]
+    Hint { msg: String, err_code: usize },
 
     #[error("timeout")]
     Timeout,
@@ -76,20 +76,11 @@ use actix_web::{http::header::ContentType, HttpResponse};
 impl actix_web::error::ResponseError for ErrorKind {
     fn status_code(&self) -> actix_web::http::StatusCode {
         match self {
-            ErrorKind::Business {
-                msg: _,
-                err_code: _,
-            } => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
-            ErrorKind::Validate {
-                msg: _,
-                err_code: _,
-            } => actix_web::http::StatusCode::BAD_REQUEST,
-            ErrorKind::Unauthorized {
-                msg: _,
-                err_code: _,
-            } => actix_web::http::StatusCode::UNAUTHORIZED,
+            ErrorKind::Business { .. } => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorKind::Validate { .. } => actix_web::http::StatusCode::BAD_REQUEST,
+            ErrorKind::Unauthorized { .. } => actix_web::http::StatusCode::UNAUTHORIZED,
+            ErrorKind::Hint { .. } => actix_web::http::StatusCode::BAD_REQUEST,
             ErrorKind::Timeout => actix_web::http::StatusCode::REQUEST_TIMEOUT,
-            ErrorKind::Hint(_) => actix_web::http::StatusCode::from_u16(452).unwrap(),
             _ => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
@@ -107,13 +98,11 @@ impl actix_web::error::ResponseError for ErrorKind {
             .body(match self {
                 ErrorKind::Business { msg, err_code }
                 | ErrorKind::Validate { msg, err_code }
+                | ErrorKind::Hint { msg, err_code }
                 | ErrorKind::Unauthorized { msg, err_code } => serde_json::to_string(
                     &util_response::ErrorResponse::new(&format!("{}", msg), Some(*err_code)),
                 )
                 .unwrap(),
-                ErrorKind::Hint(msg) => {
-                    serde_json::to_string(&util_response::MsgResponse::new(msg)).unwrap()
-                }
                 _ => serde_json::to_string(&util_response::MsgResponse::new(&self.to_string()))
                     .unwrap(),
             })
@@ -161,7 +150,13 @@ macro_rules! validate_error {
 #[macro_export]
 macro_rules! hint {
     ($msg: expr) => {{
-        let res = util_error::ErrorKind::Hint($msg.to_string());
+        hint!($msg, util_error::ErrCode::Hint as usize)
+    }};
+    ($msg: expr, $err_code: expr) => {{
+        let res = util_error::ErrorKind::Hint {
+            msg: $msg.to_string(),
+            err_code: $err_code,
+        };
         log::warn!("{}", res);
         res
     }};
